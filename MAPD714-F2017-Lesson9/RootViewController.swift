b03594c84c35c7b70ade3c8f128586387d866da6
Student <student@setass-mac-mini-2.local>
2017-11-30 19:39:38 -0500

    Code cleanup

--- MAPD714-F2017-Lesson9/RootViewController.swift
+++ MAPD714-F2017-Lesson9/RootViewController.swift
@@ -33,9 +33,6 @@ class RootViewController: UITableViewController {
             let fontName = UIFont.fontNames(forFamilyName: familyName).first
             return fontName != nil ? UIFont(name: fontName!, size: cellPointSize) : nil
         } else {
-            //let familyName = favouritesList.favourites[indexPath.row]
-            //let fontName = UIFont.fontNames(forFamilyName: familyName).first
-            //return fontName != nil ? UIFont(name: fontName!, size: cellPointSize) : nil
             return nil
         }
     }
@@ -65,11 +62,7 @@ class RootViewController: UITableViewController {
             return cell
         } else {
             // the favourites list
-            //return tableView.dequeueReusableCell(withIdentifier: RootViewController.favouritesCell, for: indexPath)
             let cell = tableView.dequeueReusableCell(withIdentifier: RootViewController.favouritesCell, for: indexPath)
-            //cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath as NSIndexPath)
-            //cell.textLabel?.text = familyNames[indexPath.row]
-            //cell.detailTextLabel?.text = familyNames[indexPath.row]
             return cell
         }
     }
